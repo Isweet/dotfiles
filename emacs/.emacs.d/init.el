72939c2932ea56c06a3138b150c48acf57bf5125
Ian Sweet <iannsweet@gmail.com>
2017-03-18 15:59:50 -0400

    update emacs config

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,101 +1,58 @@
+;; Setup package manager
 (require 'package)
-(setq package-enable-at-startup nil)
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
 (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
-(add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/"))
 (package-initialize)
 
+;;; If use-package doesn't exist, make it exist
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
+(require 'use-package)
 
-(eval-when-compile
-  (require 'use-package))
-(require 'diminish)
-(require 'bind-key)
+;; Install packages
 
-;; Packages
-(require 'package)
+;;; Monokai Theme
+(use-package monokai-theme)
 
-(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
-(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
-(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/"))
-
-(package-initialize)
-
-(setq package-list '())
-
-; Monokai color scheme
-
-(add-to-list 'package-list 'monokai-theme)
-
-; Vim
-
-(add-to-list 'package-list 'evil)
-
-; Haskell mode
-
-(add-to-list 'package-list 'haskell-mode)
-
-; OCaml mode
-
-(add-to-list 'package-list 'tuareg)
-
-; Coq
-
-(load-file "/usr/local/Cellar/proof-general/4.4/share/emacs/site-lisp/proof-general/generic/proof-site.el")
-(add-hook 'proof-ready-for-assistant-hook (lambda () (show-paren-mode 0)))
-(add-hook 'coq-mode-hook (lambda () (local-set-key (kbd "C-c RET") 'proof-goto-point)))
-
-;; Install
-(unless package-archive-contents
-  (package-refresh-contents))
-
-(dolist (package package-list)
-  (unless (package-installed-p package)
-    (package-install package)))
+;;; Haskell
+(use-package haskell-mode)
 
 ;; UI
 (load-theme 'monokai t)
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 
-;; Default Packages
-
-; Evil
-(setq evil-want-C-u-scroll t)
-(require 'evil)
-(evil-mode t)
-
 ;; Sensible defaults
-(setq-default indent-tabs-mode nil)
-(setq-default c-basic-indent 2)
-(setq tab-width 2)
 
-(setq linum-format " %d ")
+;;; No tabs
+(setq-default indent-tabs-mode nil)
+
+;;; 2 spaces instead
+(setq-default tab-width 2)
+
+;;; I want line numbers, and current line highlighted
+(setq-default linum-format " %d ")
 (global-linum-mode t)
 (global-hl-line-mode 1)
 (set-face-background 'hl-line "#262626")
 (set-face-foreground 'highlight nil)
 
-(setq make-backup-files nil)
-(setq auto-save-default nil)
+;;; No annoying files, no automatically saving
+(setq-default make-backup-files nil)
+(setq-default auto-save-default nil)
 
-(setq vc-follow-symlinks t)
+;;; When I open a symlink, open the file pointed to
+(setq-default vc-follow-symlinks t)
 
+;;; Smoother scrolling?
 (setq scroll-conservatively 101)
 
+;;; Show column number in status bar
 (setq column-number-mode t)
+
+;;; Highlight matching parens
 (show-paren-mode 1)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (monokai-theme evil haskell-mode tuareg))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+
+;; When in Haskell, TeX input
+;; (add-hook 'haskell-mode-hook (lambda () (set-input-method "TeX")))
